0277f5d1eb5a3820d620e109b9b6f3fa3a660492
Chris Richards <rdchris@gmail.com>
2023-02-27 12:54:04 -0600

    WIP

--- src/main/java/com/earlyfork/tiledgidbatch/TiledgidbatchApplication.java
+++ src/main/java/com/earlyfork/tiledgidbatch/TiledgidbatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,6 +14,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 
@@ -34,12 +36,11 @@ public class TiledgidbatchApplication implements CommandLineRunner {
 
         //update
         XPath xPath = XPathFactory.newInstance().newXPath();
-        Node startDateNode = (Node) xPath.compile("/data/startdate").evaluate(doc, XPathConstants.NODE);
-        startDateNode.setTextContent("29/07/2015");
+        Node startDateNode = (Node) xPath.compile("/map/tileset").evaluate(doc, XPathConstants.NODE);
+        Node firstgid = startDateNode.getAttributes().getNamedItem("firstgid");
+        firstgid.setNodeValue("0");
+        startDateNode.getAttributes().setNamedItem(firstgid);
 
-        xPath = XPathFactory.newInstance().newXPath();
-        Node endDateNode = (Node) xPath.compile("/data/enddate").evaluate(doc, XPathConstants.NODE);
-        endDateNode.setTextContent("29/07/2015");
 
         //Then save the Document back to the file...
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
