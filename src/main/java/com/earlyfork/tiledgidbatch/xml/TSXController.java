6532c1c7b2ead4b425af99d605a1ffd329047327
Chris Richards <rdchris@gmail.com>
2023-02-28 23:02:49 -0600

    lombok

--- src/main/java/com/earlyfork/tiledgidbatch/xml/TSXController.java
+++ src/main/java/com/earlyfork/tiledgidbatch/xml/TSXController.java
@@ -1,6 +1,7 @@
 package com.earlyfork.tiledgidbatch.xml;
 
 import com.earlyfork.tiledgidbatch.pojos.TilesetChangeset;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
